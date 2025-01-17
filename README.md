bcf79855766664fd280b5cfa0059e717648a4165
Ansel Rognlie <ansel@adadevelopersacademy.org>
2025-01-16 16:03:28 -0800

    initial commit ported from replit

---
+++ README.md
@@ -0,0 +1,77 @@
+# Rotate Problem
+
+Problem belonging to the post-classroom Mock Interview Question Repository.
+
+## Problem Statement
+
+Our goal is to write a function that will rotate a linked list by `k` places to the right.
+
+For example,if we have the following input list:
+
+a → b → c → d → e
+
+After rotating by a `k` value of 2, we should get the following output list:
+
+d → e → a → b → c
+
+Notice that everything has been moved 2 spaces to the right. Anything that reaches the end of the list is wrapped back around to the beginning.
+
+The value 'a' was originally in the 0th position (the head of the list), so when it is shifted by two, it ends up in the 2nd position.
+
+The value 'e' was originally in the 4th position (the end of the list). When it is shifted two places, it wraps to the beginning of the list (position 0) and then moves one additional space, ending at the 1st position.
+
+`k` is guaranteed to be positive.
+
+Adapted from https://leetcode.com/problems/rotate-list/
+
+## Examples
+
+### Example 1
+
+Rotating the following list
+
+a → b → c → d → e
+
+by 1 produces
+
+e → a → b → c → d
+
+### Example 2
+
+Rotating the following list
+
+a → b → c → d → e
+
+by 2 produces
+
+d → e → a → b → c
+
+### Example 3
+
+Rotating the following list
+
+a → b → c → d → e
+
+by 10 produces
+
+a → b → c → d → e
+
+### Example 4
+
+Rotating the following list
+
+55 → 8 → 2 → 99
+
+by 6 produces
+
+2 → 99 → 55 → 8
+
+### Example 5
+
+Rotating the following list
+
+1 → 2
+
+by 5 produces
+
+2 → 1
---
+++ main.py
@@ -0,0 +1,62 @@
+# DO NOT MODIFY THE NODE CLASS
+class Node:
+    def __init__(self, value, next=None):
+        self.value = value 
+        # if next is None, this is the last element in the list
+        self.next = next
+
+    def __eq__(self, other):
+        '''
+        Understanding this function is NOT necessary for solving the problem;
+        it is only used for the assertions.
+        Feel free to explore your curiosity of how this works after the interview :)
+        '''
+        try:
+            return (type(other) == Node and 
+                    self.value == other.value and 
+                    self.next == other.next)
+        except RecursionError:
+            raise Exception("Linked list has a cycle or is too large")
+
+def rotate(head, k):
+    # Your implementation here!
+    pass
+
+
+# Input list: a->b->c->d->e
+# Rotate by 1
+# Output list: e->a->b->c->d
+list_1 = Node('a', Node('b', Node('c', Node('d', Node('e')))))
+expected = Node('e', Node('a', Node('b', Node('c', Node('d')))))
+assert rotate(list_1, 1) == expected
+
+# Input list: a->b->c->d->e
+# Rotate by 2
+# Output list: d->e->a->b->c
+list_2 = Node('a', Node('b', Node('c', Node('d', Node('e')))))
+expected = Node('d', Node('e', Node('a', Node('b', Node('c')))))
+assert rotate(list_2, 2) == expected
+
+# Input list: a->b->c->d->e
+# Rotate by 10
+# Input list: a->b->c->d->e
+list_3 = Node('a', Node('b', Node('c', Node('d', Node('e')))))
+expected = Node('a', Node('b', Node('c', Node('d', Node('e')))))
+assert rotate(list_3, 10) == expected
+
+# Input list: 55->8->2->99
+# Rotate by 6
+# Output list: 2->99->55->8
+list_4 = Node(55, Node(8, Node(2, Node(99))))
+expected = Node(2, Node(99, Node(55, Node(8))))
+assert rotate(list_4, 6) == expected
+
+# Input list: 1->2
+# Rotate by 5
+# Output list: 2->1
+list_5 = Node(1, Node(2))
+expected = Node(2, Node(1))
+assert rotate(list_5, 5) == expected
+
+print("All tests passed!")
+print("Discuss time & space complexity if time remains.")
